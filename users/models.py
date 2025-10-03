b89e417e3bd2c40cbf3215e9b9d4b561709c539a
Sjacuru <sjacuru@gmail.com>
2025-10-03 23:48:47 +1300

    BMS-6: Add User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=255)
+    role = models.CharField(max_length=10, choices=[('manager', 'Manager'), ('staff', 'Staff')])
+
+    class Meta:
+        db_table = 'Users'
